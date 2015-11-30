6b121cc11f137bc42407fde030015ab416c2177f
yuly-sanchez <yuly.sanchez29@gmail.com>
2015-11-30 17:22:20 +0100

    first commit

---
+++ src/introsde/document/endpoint/PeoplePublisher.java
@@ -0,0 +1,29 @@
+package introsde.document.endpoint;
+import introsde.document.ws.PeopleImpl;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.io.IOException;
+
+import javax.xml.ws.Endpoint;
+
+public class PeoplePublisher {
+	
+    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
+        String PROTOCOL = "http://";
+        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+        if (HOSTNAME.equals("127.0.0.1")){
+            HOSTNAME = "localhost";
+        }
+        String PORT = "6902";
+        String BASE_URL = "/ws/people";
+
+        if (String.valueOf(System.getenv("PORT")) != "null"){
+            PORT=String.valueOf(System.getenv("PORT"));
+        }
+        
+        String endpointUrl = PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL;
+        System.out.println("Starting People Service...");
+        System.out.println("--> Published. Check out " + endpointUrl + "?wsdl");
+        Endpoint.publish(endpointUrl, new PeopleImpl());
+    }
+}
