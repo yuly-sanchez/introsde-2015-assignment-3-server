8db794f2e7663d8ddf08f8d083810d71bd1d1c2a
yuly-sanchez <yuly.sanchez29@gmail.com>
2015-12-03 00:30:39 +0100

    first commit

--- src/introsde/document/model/Measure.java
+++ src/introsde/document/model/Measure.java
@@ -17,11 +17,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -30,8 +32,14 @@ import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @Entity  
-@Table(name="Measure")  
-@NamedQuery(name="Measure.findAll", query="SELECT m FROM Measure m")
+@Table(name="Measure")
+@NamedQueries({
+	@NamedQuery(name="Measure.findAll", query="SELECT m FROM Measure m"),
+	@NamedQuery(name="Measure.findByMeasureType", query="SELECT m FROM Measure m WHERE m.person = ?1 AND m.measureType = ?2"),
+	@NamedQuery(name="Measure.findMeasureTypes", query="SELECT DISTINCT m.measureType FROM Measure m"),
+	@NamedQuery(name="Measure.findByMeasureMid", query="SELECT m FROM Measure m WHERE m.person = ?1 AND m.measureType = ?2 AND m.idMeasure =?3")
+})
+
 @XmlAccessorType(XmlAccessType.FIELD)
 //@XmlRootElement
 public class Measure implements Serializable{
@@ -166,4 +174,39 @@ public class Measure implements Serializable{
         LifeCoachDao.instance.closeConnections(em);
     }
     
+    /**
+	 * Returns the history of a measureType for a person
+	 * @param p
+	 * @param md
+	 * @return list of HealthMeasureHistory
+	 */
+	public static List<Measure> getByPersonMeasure(Person p, String measureType) {
+		EntityManager em = LifeCoachDao.instance.createEntityManager();
+		TypedQuery<Measure> query = em.createNamedQuery("Measure.findByMeasureType", Measure.class);
+		query.setParameter(1, p);
+		query.setParameter(2, measureType);
+		List<Measure> list = query.getResultList();
+	    LifeCoachDao.instance.closeConnections(em);
+		return list;
+	}
+	
+	
+	public static List<String> getByMeasureTypes() {
+		EntityManager em = LifeCoachDao.instance.createEntityManager();
+        List<String> list = em.createNamedQuery("Measure.findMeasureTypes", String.class)
+        		.getResultList();
+        LifeCoachDao.instance.closeConnections(em);
+        return list;
+	}
+	
+	public static Measure getByPersonMid(Person p, String measureType, Long idMeasure){
+		EntityManager em = LifeCoachDao.instance.createEntityManager();
+        Measure measure = em.createNamedQuery("Measure.findByMeasureMid", Measure.class)
+        		.setParameter(1, p)
+        		.setParameter(2, measureType)
+        		.setParameter(3, idMeasure)
+        		.getSingleResult();
+        LifeCoachDao.instance.closeConnections(em);
+        return measure;
+	}
 }
