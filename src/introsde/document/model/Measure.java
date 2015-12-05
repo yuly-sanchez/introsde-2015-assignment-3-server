e31f531f1785b2dc5008a623443b4160f2e6511f
yuly-sanchez <yuly.sanchez29@gmail.com>
2015-12-05 21:55:32 +0100

    second commit

--- src/introsde/document/model/Measure.java
+++ src/introsde/document/model/Measure.java
@@ -5,6 +5,7 @@ import introsde.document.converter.DateConverter;
 import introsde.document.dao.LifeCoachDao;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -37,10 +38,11 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 @Table(name = "Measure")
 @NamedQueries({
 		@NamedQuery(name = "Measure.findAll", query = "SELECT m FROM Measure m"),
-		@NamedQuery(name = "Measure.findByMeasureType", query = "SELECT m FROM Measure m WHERE m.person = ?1 AND m.measureType = ?2"),
+		@NamedQuery(name = "Measure.findByPersonId", query = "SELECT m FROM Measure m WHERE m.person = ?1"),
+		@NamedQuery(name = "Measure.findByMeasureType", query = "SELECT m FROM Measure m WHERE m.person = ?1 AND m.measureType = ?2 AND m.isCurrent = 0"),
 		@NamedQuery(name = "Measure.findMeasureTypes", query = "SELECT DISTINCT m.measureType FROM Measure m"),
 		@NamedQuery(name = "Measure.findByMeasureMid", query = "SELECT m FROM Measure m WHERE m.person = ?1 AND m.measureType = ?2 AND m.idMeasure =?3"),
-		@NamedQuery(name = "Measure.getMeasureByMeasureType", query = "SELECT m.measureValueType FROM Measure m WHERE m.measureType = ?1 AND m.person = ?2") })
+		@NamedQuery(name = "Measure.getCurrentMeasure", query = "SELECT m FROM Measure m WHERE m.person = ?1 AND m.measureType = ?2 AND m.isCurrent = 1") })
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
 public class Measure implements Serializable {
@@ -66,8 +68,13 @@ public class Measure implements Serializable {
 	@Column(name = "measureValue")
 	private String measureValue;
 
-	@Column(name = "measureValueType")
-	private String measureValueType;
+	@Column(name = "valueType")
+	@XmlElement(name="measureValueType")
+	private String valueType;
+
+	@Column(name="isCurrent")
+	@XmlTransient
+	private int isCurrent; // 1 current Health, 0 history Health
 
 	@ManyToOne
 	@JoinColumn(name = "idPerson", referencedColumnName = "idPerson")
@@ -76,11 +83,11 @@ public class Measure implements Serializable {
 	
 	public Measure(){}
 	
-	public Measure(Date dateRegistered, String measureType, String measureValue, String measureValueType, Person person){
-		this.dateRegistered = dateRegistered;
-		this.measureType = measureType;
-		this.measureValue = measureValue;
-		this.measureValueType = measureValueType;
+	public Measure(Date dRegistered, String mType, String mValue, String mValueType, Person person){
+		this.dateRegistered = dRegistered;
+		this.measureType = mType;
+		this.measureValue = mValue;
+		this.valueType = mValueType;
 		this.person = person;
 	}
 
@@ -101,10 +108,14 @@ public class Measure implements Serializable {
 		return measureValue;
 	}
 
-	public String getMeasureValueType() {
-		return measureValueType;
+	public String getValueType() {
+		return valueType;
 	}
 
+	public int getIsCurrent() {
+		return isCurrent;
+	}
+	
 	public Person getPerson() {
 		return person;
 	}
@@ -126,17 +137,21 @@ public class Measure implements Serializable {
 		this.measureValue = measureValue;
 	}
 
-	public void setMeasureValueType(String measureValueType) {
-		this.measureValueType = measureValueType;
+	public void setValueType(String valueType) {
+		this.valueType = valueType;
 	}
 
+	public void setIsCurrent(int isCurrent) {
+		this.isCurrent = isCurrent;
+	}
+	
 	public void setPerson(Person person) {
 		this.person = person;
 	}
 
 	public String toString() {
-		return "Measure ( " + idMeasure + " " + dateRegistered + " "
-				+ measureType + " " + measureValue + " " + measureValueType
+		return "Measure ( " + idMeasure + ", " + dateRegistered + ", "
+				+ measureType + ", " + measureValue + ", " + valueType + ", " + isCurrent
 				+ " )";
 	}
 
@@ -187,13 +202,26 @@ public class Measure implements Serializable {
 	}
 
 	/**
+	 * Returns the list of the measures associated of one person
+	 * @param p
+	 * @return
+	 */
+	public static List<Measure> getAllMeasures(Person p) {
+		EntityManager em = LifeCoachDao.instance.createEntityManager();
+		List<Measure> measuresList = em.createNamedQuery("Measure.findByPersonId", Measure.class)
+				.setParameter(1, p).getResultList();
+		LifeCoachDao.instance.closeConnections(em);
+		return measuresList;
+	}
+	
+	
+	/**
 	 * Returns the history of a measureType for a person
-	 * 
 	 * @param p
 	 * @param md
 	 * @return list of HealthMeasureHistory
 	 */
-	public static List<Measure> getByPersonMeasure(Person p, String measureType) {
+	public static List<Measure> getHistoryMeasureByPerson(Person p, String measureType) {
 		EntityManager em = LifeCoachDao.instance.createEntityManager();
 		TypedQuery<Measure> query = em.createNamedQuery(
 				"Measure.findByMeasureType", Measure.class);
@@ -212,35 +240,101 @@ public class Measure implements Serializable {
 		return list;
 	}
 
-	public static Measure getByPersonMid(Person p, String measureType,
-			Long idMeasure) {
+	public static Measure getByPersonMid(Person p, String measureType,Long idMeasure) {
 		EntityManager em = LifeCoachDao.instance.createEntityManager();
 		Measure measure = em
 				.createNamedQuery("Measure.findByMeasureMid", Measure.class)
-				.setParameter(1, p).setParameter(2, measureType)
+				.setParameter(1, p)
+				.setParameter(2, measureType)
 				.setParameter(3, idMeasure).getSingleResult();
 		LifeCoachDao.instance.closeConnections(em);
 		return measure;
 	}
 
 	/**
-	 * Given a measure name, the function returns the corresponding Measure
+	 * Given a measure type and personId, the function returns the corresponding Measure
 	 * object.
-	 * 
-	 * @param measureName
+	 * @param person
+	 * @param measureType
 	 * @return Measure
 	 */
-	public static String getMeasureByMeasureType(String measureType, Person person) {
+	public static Measure foundCurrentMeasure(Person person, String measureType) {
 		EntityManager em = LifeCoachDao.instance.createEntityManager();
 		try {
-			String measureValueType = em.createNamedQuery("Measure.getMeasureByMeasureType", String.class)
-						.setParameter(1, measureType)
-						.setParameter(2, person)
+			Measure measure = em.createNamedQuery("Measure.getCurrentMeasure", Measure.class)
+						.setParameter(1, person)
+						.setParameter(2, measureType)
 						.getSingleResult();
 			LifeCoachDao.instance.closeConnections(em);
-			return measureValueType;
+			return measure;
 		} catch (Exception e) {
 			return null;
 		}
+	}
+	
+	public static Measure newMeasureValue(Long id, Measure m){
+		
+		Measure target = null;
+		Measure result = null;
+		//set today date
+		Calendar calendar = Calendar.getInstance();
+		
+		//search the person by idPerson 
+		Person person = Person.getPersonById(id);
+		
+		//check if found person with this idPerson
+		if(person != null){
+			System.out.println("--> Found person with id: " + id);
+			
+			//check if measure already exits for person identified by id 
+			target = foundCurrentMeasure(person, m.getMeasureType());
+			
+			if(target != null){
+				System.out.println("--> Found measure...");
+				target.setIsCurrent(0);
+				target.setIdMeasure(target.getIdMeasure());
+				target.setDateRegistered(target.getDateRegistered());
+				target.setMeasureType(target.getMeasureType());
+				target.setMeasureValue(target.getMeasureValue());
+				target.setValueType(target.getValueType());
+				target.setPerson(person);
+				Measure.updateMeasure(target);
+				
+				
+				//create a new measure that the client want
+				Measure newMeasure = new Measure();
+				newMeasure.setIsCurrent(1);
+				newMeasure.setDateRegistered(calendar.getTime());
+				newMeasure.setMeasureType(m.getMeasureType());
+				newMeasure.setMeasureValue(m.getMeasureValue());
+				newMeasure.setValueType(m.getValueType());
+				newMeasure.setPerson(person);
+				
+				//save the new measure
+				Measure.saveMeasure(newMeasure);
+				
+				System.out.println("--> measure added");
+				result = Measure.getMeasureById(newMeasure.getIdMeasure());
+			
+			}else{
+				target = new Measure();
+				target.setIsCurrent(1);
+				target.setDateRegistered(calendar.getTime());
+				target.setMeasureType(m.getMeasureType());
+				target.setMeasureValue(m.getMeasureValue());
+				target.setValueType(m.getValueType());
+				target.setPerson(person);
+				
+				System.out.println("--> measure created...");
+				
+				//save the new measure
+				Measure.saveMeasure(target);
+				
+				System.out.println("--> measure added");
+				result = Measure.getMeasureById(target.getIdMeasure());
+			}
+		
+		}
+		return result;
 	}
 }
